99f9931fd229e0a83161ffe3589d998f5946286e
Eversmile12 <72762629+Eversmile12@users.noreply.github.com>
2021-03-30 20:20:38 +0200

    Update storage.js

--- src/storage.js
+++ src/storage.js
@@ -1,13 +1,4 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyBz6HqJAKkPE0K9May5cdw9sFBW2K7i5f0",
-    authDomain: "bookmarkit-26f28.firebaseapp.com",
-    databaseURL: "https://bookmarkit-26f28-default-rtdb.firebaseio.com",
-    projectId: "bookmarkit-26f28",
-    storageBucket: "bookmarkit-26f28.appspot.com",
-    messagingSenderId: "92066820035",
-    appId: "1:92066820035:web:4fc99df169ba26a9d89776"
-  }
-
+var firebaseConfig = 
   firebase.initializeApp(firebaseConfig);
 
 
